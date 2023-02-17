7e52adec1b50b4c4dce6eb5d0f56c01fd73cdae2
RadekRo <r.roclawski@ankara.pl>
2023-02-17 22:03:53 +0100

    Created Hero class for heroes creation

---
+++ duel.py
@@ -0,0 +1,8 @@
+def fight(first_enemy, second_enemy):
+    
+    start_fight = input("Do you want them to enter the arena?\nEnter 'Y' for yes. Any other key will leave the game: ")
+  
+    if start_fight == "Y":
+        print("Fight!")
+    else:
+        print("Fight rejected. Thank you. Have a nice day!")
--- fight-arena.py
+++ fight-arena.py
@@ -1,6 +1,7 @@
 import random
-from heroes import Hero
 from os import system, name
+from heroes import Hero
+from duel import fight
 
 def clear():
   # for windows
@@ -14,4 +15,8 @@ def clear():
 names = ["James", "Conan", "Eric", "Bertrand", "Jason", "Jacob", "George", "Adam", "Sylvo", "Siergiej", "Marek", "Robert", "Simon", "Bob", "Caspar", "Slavko", "Neo", "Harold", "Kirsten"]
 
 p1 = Hero(random.choice(names))
-print(p1)
+p2 = Hero(random.choice(names))
+print("These are the today's gladiators:\n*********************************")
+p1.present_yourself()
+p2.present_yourself()
+fight(p1, p2)
