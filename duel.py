d4f60565a01b4f66cb8a3f899717f4468551f39d
RadekRo <r.roclawski@ankara.pl>
2023-02-17 22:37:23 +0100

    Started to work on fight function

--- duel.py
+++ duel.py
@@ -1,8 +1,18 @@
 def fight(first_enemy, second_enemy):
-    
+
     start_fight = input("Do you want them to enter the arena?\nEnter 'Y' for yes. Any other key will leave the game: ")
-  
-    if start_fight == "Y":
-        print("Fight!")
+
+    if start_fight.upper() == "Y":
+        turn = 1
+        while True:
+            if turn%2 == 1:
+                fighter = first_enemy
+                defender = second_enemy
+            else:
+                fighter = second_enemy
+                defender = first_enemy
+            print(fighter.name)
+            print(defender.name)
+            turn += 1
     else:
         print("Fight rejected. Thank you. Have a nice day!")
