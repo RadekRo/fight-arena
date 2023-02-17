6ad39e722a4bbbcdfba6ff33ff72593636afd7a7
RadekRo <r.roclawski@ankara.pl>
2023-02-17 21:57:19 +0100

    Added clear function for fighting purposes

--- fight-arena.py
+++ fight-arena.py
@@ -1,5 +1,15 @@
 import random
 from heroes import Hero
+from os import system, name
+
+def clear():
+  # for windows
+  if name == 'nt':
+    _ = system('cls')
+
+  # for mac and linux(here, os.name is 'posix')
+  else:
+    _ = system('clear')
 
 names = ["James", "Conan", "Eric", "Bertrand", "Jason", "Jacob", "George", "Adam", "Sylvo", "Siergiej", "Marek", "Robert", "Simon", "Bob", "Caspar", "Slavko", "Neo", "Harold", "Kirsten"]
 
